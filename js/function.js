f8177fcd2a6a8d08996ad71d53ff626ba5efb999
Brian Bauska <brianbauska@gmail.com>
2023-04-29 20:30:45 -0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,22 +1,22 @@
-/** Called from index.html within bbauska/lfs171x.bauska.org/main.  Playing and learning JS.  */
-/* ------------------------------------------------------ */
-/* ------------ script function: scrollFunction() ------- */
+/* Called from index.html within bbauska/w3cx-1of5.bauska.org/main.  Playing and learning JS. */
+/*--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--*/
+/*--~~~~~~~~~~~~~~~~~~~~~~~~~~ script function: scrollFunction() ~~~~~~~~~~~~~~~~~~~~~~~~~~~--*/
 
-//Get the button
+// Get the button.
 var mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+// When the user scrolls down 20px from the top of the document, show the button.  10 and 20.
 function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on the button, scroll to the top of the document, 0.
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
